0227b8720e7d063ef3a719862e2c9eff37173579
Abhik Bhattacharjee <abhikbhattacharjee.mit@gmail.com>
2023-03-09 11:27:45 +0000

    VI - Menu plus Logic changes

--- mdpInit.py
+++ mdpInit.py
@@ -29,11 +29,17 @@ class mdpVI():
 
         for k, v in self.ACTIONS.items():
             self.ACTIONS[k] = dict(v)
-            
+        
         for key, val in self.ACTIONS.items():
-            LENGTH = len(val.keys())
-            for dire, value in val.items():
-                self.ACTIONS[key][dire] /= LENGTH
+            for k, v in val.items():
+                if k == 'N':
+                    val[k] = 0.80
+                elif k == 'W':
+                    val[k] = 0.1
+                elif k == 'E':
+                    val[k] = 0.05
+                elif k == 'S':
+                    val[k] = 0.05
                 
         self.U = {state: 0 for state in self.ACTIONS.keys()}
         self.U[self.target[0]] = 1
@@ -58,16 +64,17 @@ class mdpVI():
             for state in ACTIONS.keys():
                 if state == target[0]:
                     continue
-                max_utility = float("-inf")
+                max_utility = float("-infinity")
                 max_action = None
                 for action, prob in ACTIONS[state].items():
                     for direction in action:
                         if mazeDef.maze_map[state][direction]==True:  
                             next_state = self.mazeTrace(mazeDef, state, direction)
+                    utility = 0
                     reward = REWARD
                     if next_state == target[0]:
                         reward = 1
-                    utility = prob * (reward + DISCOUNT * U[next_state])
+                    utility += prob * (reward + DISCOUNT * U[next_state])
                     if utility > max_utility:
                         max_utility = utility
                         max_action = action
@@ -86,7 +93,7 @@ class mdpVI():
             bestNodeVal = None
             if node == self.target[0]:
                 break
-            for direction in 'EWNS':
+            for direction in 'NWSE':
                 if mazeDef.maze_map[node][direction] == True and self.mazeTrace(mazeDef, node, direction) not in GLOBAL_VISITED:
                     directionalNode =  self.mazeTrace(mazeDef, node, direction)
                     if  directionalNode == self.target[0]:
@@ -101,28 +108,65 @@ class mdpVI():
                         if bestNodeVal < self.U[tempNode]:
                             bestNode = tempNode
                             bestNodeVal = self.U[tempNode]
-                    GLOBAL_VISITED.append(node)
+            GLOBAL_VISITED.append(bestNode)
             self.tracePath[node] = bestNode
             node = bestNode
         return self.tracePath, U, policy
     
 if __name__=='__main__':
-    print("\n Enter maze size: \n")
-    ipx = input("\n X: ")
-    x = int(ipx)
-    ipy = input("\n Y: ")
-    y = int(ipy)
-    print("\n Enter Target: \n")
-    tx = input("\n X: ")
-    tarx = int(tx)
-    ty = input("\n Y: ")
-    tary = int(ty)
+    print("\n Please select Maze: \n")
+    print(" 1. 5x5 Maze\n")
+    print(" 2. 30x30 Maze\n")
+    print(" 3. 50x50 Maze\n")
+    print(" 4. 100x100 Maze\n")
+    print(" 5. 25x50 Maze\n")
+    menu1 = input("\n Enter Choice: ")
+    choice = int(menu1)
     
-    mazeDef = maze(x, y)
-    mazeDef.CreateMaze(tarx, tary, loopPercent = 100, theme = COLOR.light)
-    
-    track, U, policy = mdpVI().mazeTrack((x,y), tarx, tary, -1, 0.9, 10**(-3), mazeDef)
-    agent1 = agent(mazeDef, shape = 'arrow', footprints = True, color = COLOR.red)
-    mazeDef.tracePath({agent1: track}, delay=200)
-    mazeDef.run()
-    
+    if choice == 1:
+        print(" Selected 5x5 Maze\n")
+        print(" Default Target : (1,1)\n Reward : 4\n Discount : 0.8\n Error : 0.001")
+        mazeDef = maze(5,5)
+        mazeDef.CreateMaze(loadMaze='./Maze/maze5.csv')
+        track, U, policy = mdpVI().mazeTrack((5,5), 1, 1, -4, 0.8, 10**(-3), mazeDef)
+        agent1 = agent(mazeDef, shape = 'arrow', footprints = True, color = COLOR.red)
+        mazeDef.tracePath({agent1: track}, delay=200)
+        mazeDef.run()
+    elif choice == 2:
+        print(" Selected 30x30 Maze\n")
+        print(" Default Target : (1,1)\n Reward : 4\n Discount : 0.8\n Error : 0.001")
+        mazeDef = maze(30,30)
+        mazeDef.CreateMaze(loadMaze='./Maze/maze30.csv')
+        track, U, policy = mdpVI().mazeTrack((30,30), 1, 1, -4, 0.8, 10**(-3), mazeDef)
+        agent1 = agent(mazeDef, shape = 'arrow', footprints = True, color = COLOR.red)
+        mazeDef.tracePath({agent1: track}, delay=200)
+        mazeDef.run()
+    elif choice == 3:
+        print(" Selected 50x50 Maze\n")
+        print(" Default Target : (1,1)\n Reward : 4\n Discount : 0.8\n Error : 0.001")
+        mazeDef = maze(50,50)
+        mazeDef.CreateMaze(loadMaze='./Maze/maze50.csv')
+        track, U, policy = mdpVI().mazeTrack((50,50), 1, 1, -4, 0.8, 10**(-3), mazeDef)
+        agent1 = agent(mazeDef, shape = 'arrow', footprints = True, color = COLOR.red)
+        mazeDef.tracePath({agent1: track}, delay=200)
+        mazeDef.run()
+    elif choice == 4:
+        print(" Selected 100x100 Maze\n")
+        print(" Default Target : (1,1)\n Reward : 4\n Discount : 0.8\n Error : 0.001")
+        mazeDef = maze(100,100)
+        mazeDef.CreateMaze(loadMaze='./Maze/maze100.csv')
+        track, U, policy = mdpVI().mazeTrack((100,100), 1, 1, -4, 0.8, 10**(-3), mazeDef)
+        agent1 = agent(mazeDef, shape = 'arrow', footprints = True, color = COLOR.red)
+        mazeDef.tracePath({agent1: track}, delay=200)
+        mazeDef.run()
+    elif choice == 5:
+        print(" Selected 25x50 Maze\n")
+        print(" Default Target : (1,1)\n Reward : 4\n Discount : 0.8\n Error : 0.001")
+        mazeDef = maze(25,50)
+        mazeDef.CreateMaze(loadMaze='./Maze/maze2550.csv')
+        track, U, policy = mdpVI().mazeTrack((25,50), 1, 1, -4, 0.8, 10**(-3), mazeDef)
+        agent1 = agent(mazeDef, shape = 'arrow', footprints = True, color = COLOR.red)
+        mazeDef.tracePath({agent1: track}, delay=200)
+        mazeDef.run()
+    else:
+        print(" Wrong Option Selected! \n Exiting!\n")
